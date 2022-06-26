e6812017b4b85a03914edf4c7109a417a978cb38
Veronika Chagina <90192566+Veronika-Chagina@users.noreply.github.com>
2022-06-26 18:50:02 +0300

    Add files via upload

--- 12.py
+++ 12.py
@@ -2,20 +2,20 @@ input_file = open("input.txt")
 n = int(input_file.readline())
 seq = list(map(int, input_file.readline().split()))
 amount = sum(seq)
-if (amount % 2 == 1):
+if amount % 2 == 1:
     output_file = open("output.txt", "w")
     output_file.write("-1")
     output_file.close()
     exit()
 
 amount = amount // 2
-taked = 0
+taken = 0
 i = 0
-while ((i < len(seq)) and (taked < amount)):
-    taked += seq[i]
+while (i < len(seq)) and (taken < amount):
+    taken += seq[i]
     i += 1
 
-if (taked == amount):
+if taken == amount:
     result = ""
     takedSecondHalf = 0
     takedSecondCount = 0
@@ -23,7 +23,7 @@ if (taked == amount):
         takedSecondHalf += seq[index]
         takedSecondCount += 1
         result += f"{seq[index]} "
-    if (takedSecondHalf == taked == amount):
+    if takedSecondHalf == taken == amount:
         output_file = open("output.txt", "w")
         output_file.write(f"{takedSecondCount} \n")
         output_file.write(result)
@@ -35,5 +35,3 @@ if (taked == amount):
 output_file = open("output.txt", "w")
 output_file.write("-1")
 output_file.close()
-
-
